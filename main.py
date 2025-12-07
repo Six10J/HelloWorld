77f90ea46e61e473166851c216060afca614921c
Sixten Jansson <sixten00j@gmail.com>
2025-12-07 17:28:04 +0100

    HelloWorld

--- main.py
+++ main.py
@@ -3,3 +3,8 @@ print("Mitt namn är Sixten Jansson")
 
 Nummer = 10 * 10
 print(Nummer)
+
+name = input('Vad är ditt namn?')
+farg = input('Vad är din favorit färg ' + name + '?')
+
+print("Hej " + name + " din favorit färg är " + farg + "!")
